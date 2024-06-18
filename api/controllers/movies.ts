8162d89419dcd7cfe0d4e536df09ea28eaf775cf
Diana <diana.dev777@gmail.com>
2024-06-18 12:14:54 +0500

    feat: implement filter for genres

--- api/controllers/movies.ts
+++ api/controllers/movies.ts
@@ -1,15 +1,26 @@
-import { MovieType } from '@/common/types';
+import { GenreType, MovieType } from '@/common/types';
 import { MoviesType } from '@/common/types/movies-type';
 import { BaseController } from './base';
 
 const KINOPOISK_ENDPOINT = process.env.NEXT_PUBLIC_API_URL;
 
 export class MovieController extends BaseController {
-  public static getAllMovies() {
-    return this.get<MoviesType>(`${KINOPOISK_ENDPOINT}/movie?page=1&limit=50`);
+  public static async getAllMovies(pageNumber: number, genres: string[] = []): Promise<MoviesType> {
+    let url = `${KINOPOISK_ENDPOINT}/v1.4/movie?page=${pageNumber}&limit=50`;
+
+    if (genres.length) {
+      const genreParams = genres.map(genre => `genres.name=${encodeURIComponent(genre)}`).join('&');
+      url += `&${genreParams}`;
+    }
+
+    return this.get<MoviesType>(url);
   }
 
   public static getMovieById(filmId: string) {
-    return this.get<MovieType>(`${KINOPOISK_ENDPOINT}/movie/${filmId}`);
+    return this.get<MovieType>(`${KINOPOISK_ENDPOINT}/v1.4/movie/${filmId}`);
+  }
+
+  public static getGenres() {
+    return this.get<GenreType[]>(`${KINOPOISK_ENDPOINT}/v1/movie/possible-values-by-field?field=genres.name`);
   }
 }
